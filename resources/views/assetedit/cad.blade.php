d6b25d0a71679c923583d69f686ca1192d983b77
yarunkumar <ayarragudi@unomaha.edu>
2016-10-21 22:44:45 -0500

    Cad to cad
    change of hidden field

--- resources/views/assetedit/cad.blade.php
+++ resources/views/assetedit/cad.blade.php
@@ -2,29 +2,24 @@
 
 <div class="row">
     <div class="col-xs-6 form-group">
-        {!! Form::label('mdc_user_id', 'MDC User ID', ['class' => 'control-label']) !!}
-        {!! Form::text('mdc_user_id', old('mdc_user_id'), ['class' => 'form-control']) !!}
+        {!! Form::label('mdc_id', 'MDC User ID', ['class' => 'control-label']) !!}
+        {!! Form::text('mdc_id', old('mdc_id'), ['class' => 'form-control']) !!}
     </div>
     <div class="col-xs-6 form-group">
-        {!! Form::label('mdc_password', 'MDC Password', ['class' => 'control-label']) !!}
-        {!! Form::text('mdc_password', old('mdc_password'), ['class' => 'form-control']) !!}
+        {!! Form::label('mdc_pass', 'MDC Password', ['class' => 'control-label']) !!}
+        {!! Form::text('mdc_pass', old('mdc_pass'), ['class' => 'form-control']) !!}
     </div>
 </div>
 
 <div class="row">
+    
     <div class="col-xs-6 form-group">
-        {!! Form::label('vehicle_id', 'Assign to a vehicle', ['class' => 'control-label']) !!}
-        {!! Form::select('vehicle_id', $vehicles, old('vehicle_id'), ['class' => 'form-control']) !!}
-        <p class="help-block"></p>
-        @if($errors->has('vehicle_id'))
-            <p class="help-block">
-                {{ $errors->first('vehicle_id') }}
-            </p>
-        @endif
-    </div>
+            {!! Form::label('comments', 'Comments', ['class' => 'control-label']) !!}
+            {!! Form::text('comments', old('comments'), ['class' => 'form-control']) !!}
+        </div>
     <div class="col-xs-6 form-group">
-        {!! Form::label('unit_agency', 'Unit Agency', ['class' => 'control-label']) !!}
-        {!! Form::text('unit_agency', old('unit_agency'), ['class' => 'form-control']) !!}
+        {!! Form::label('manu', 'Unit Agency', ['class' => 'control-label']) !!}
+        {!! Form::text('manu', old('manu'), ['class' => 'form-control']) !!}
     </div>
 </div>
 
@@ -39,19 +34,35 @@
             </p>
         @endif
     </div>
-
-</div>
-<div class="row">
     <div class="col-xs-6 form-group">
-            {!! Form::label('asset_type', 'Asset Type', ['class' => 'control-label']) !!}
-            {!! Form::text('asset_type', old('asset_type'), ['class' => 'form-control']) !!}
-        </div>
-        <div class="col-xs-6 form-group">
-            {!! Form::label('comments', 'Comments', ['class' => 'control-label']) !!}
-            {!! Form::text('comments', old('comments'), ['class' => 'form-control']) !!}
-        </div>
-        
+        {!! Form::label('vehicle_id', 'Assign to a vehicle', ['class' => 'control-label']) !!}
+        {!! Form::select('vehicle_id', $vehicles, old('vehicle_id'), ['class' => 'form-control']) !!}
+        <p class="help-block"></p>
+        @if($errors->has('vehicle_id'))
+            <p class="help-block">
+                {{ $errors->first('vehicle_id') }}
+            </p>
+        @endif
     </div>
+    </div>
+    <div class="row">
+    
+        <div class="col-xs-6 form-group">
+                    {!! Form::label('status_id', 'Status', ['class' => 'control-label']) !!}
+                    {!! Form::select('status_id', $statuses, old('status_id'), ['class' => 'form-control']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('status_id'))
+                        <p class="help-block">
+                            {{ $errors->first('status_id') }}
+                        </p>
+                    @endif
+                </div>
+                <div class="col-xs-6 form-group">
+            
+            {!! Form::hidden('asset_type', 'cad', ['class' => 'form-control']) !!}
+        </div>
+</div>
+
 
 
 {!! Form::submit('Save',['class' => 'btn btn-success']) !!}
