de458bf9211d431bc9ff38d013d5b482196e2e21
yarunkumar <ayarragudi@unomaha.edu>
2016-11-15 20:02:44 -0600

    Vendor addition

--- resources/views/asset/vehicle.blade.php
+++ resources/views/asset/vehicle.blade.php
@@ -87,17 +87,21 @@
                         </p>
                     @endif
                 </div>
-                <div class="col-xs-6 form-group">
-                            {!! Form::label('grant_id', 'Grant', ['class' => 'control-label']) !!}
-                            {!! Form::select('grant_id', $grants, old('grant_id'), ['class' => 'form-control']) !!}
-                            <p class="help-block"></p>
-                            @if($errors->has('grant_id'))
-                                <p class="help-block">
-                                    {{ $errors->first('grant_id') }}
-                                </p>
-                            @endif
-                        </div>
+                
                 </div>
+                 <div class="row">
+<div class="col-xs-6 form-group">
+    {!! Form::label('vendor_id', 'Vendor', ['class' => 'control-label']) !!}
+    {!! Form::select('vendor_id', $vendors, old('vendor_id'), ['class' => 'form-control']) !!}
+    <p class="help-block"></p>
+    @if($errors->has('vendor_id'))
+        <p class="help-block">
+            {{ $errors->first('vendor_id') }}
+        </p>
+    @endif
+</div>
+
+</div>  
                         {!! Form::open(['method' => 'POST', 'route' => ['related_files.store'], 'files' => true,]) !!}
                         <div class="row">
                             <div class="col-xs-12 form-group">
@@ -112,6 +116,7 @@
                                 @endif
                             </div>
                         </div>
+                        
                         <div class="row">
                             <div class="col-xs-12 form-group">
                                 {!! Form::label('related_image', 'Related Image', ['class' => 'control-label']) !!}
