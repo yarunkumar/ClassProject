06270657adb280cadd7e1b13c13f31d175083aa4
Amartuvshin Orgodol <aorgodol@unomaha.edu>
2016-11-17 11:25:37 -0600

    grant delete bug fixed

--- resources/views/grants/index.blade.php
+++ resources/views/grants/index.blade.php
@@ -39,49 +39,47 @@
 
         <tbody>
             @if (count($grants) > 0)
-            @foreach ($grants as $grant)
+                @foreach ($grants as $grant)
+                    <tr data-entry-id="{{ $grant->id }}">
 
-            <tr data-entry-id="{{ $grant->id }}">
+                        <td>{{ $grant->grant_name }}</td>
+                        <td>{{ $grant->date_issued }}</td>
+                        <td>{{ $grant->grant_type }}</td>
+                        <td>{{ $grant->date_matures }}</td>
+                        <td>{{ $grant->grant_value }}</td>
+                        <td>{{ $grant->grant_comments }}</td>
+                        <td>
+                            <div style="float: right;">
+                            <a href="{{ route('grants.show',[$grant->id]) }}" class="btn btn-xs btn-info"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                            <a href="{{ route('grants.edit',[$grant->id]) }}" class="btn btn-xs btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                            <a type="button" data-toggle="modal" data-target="#{{ $grant->id }}" class="btn btn-xs btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                            </div>
+                        </td>
+                    </tr>
 
-                <td>{{ $grant->grant_name }}</td>
-                <td>{{ $grant->date_issued }}</td>
-                <td>{{ $grant->grant_type }}</td>
-                <td>{{ $grant->date_matures }}</td>
-                <td>{{ $grant->grant_value }}</td>
-                <td>{{ $grant->grant_comments }}</td>
-
-                <td>
-                    <div style="float: right;">
-                    <a href="{{ route('grants.show',[$grant->id]) }}" class="btn btn-xs btn-info"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                    <a href="{{ route('grants.edit',[$grant->id]) }}" class="btn btn-xs btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                    <a type="button" data-toggle="modal" data-target="#myModal" class="btn btn-xs btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                    <div class="modal fade" id="{{ $grant->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel">Warning</h4>
+                                </div>
+                                <div class="modal-body">
+                                    Are you sure you want to delete {{ $grant->grant_name }}? This action cannot be undone.
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    {!! Form::open(array(
+                                    'style' => 'display: inline-block;',
+                                    'method' => 'DELETE',
+                                    'route' => ['grants.destroy', $grant->id])) !!}
+                                    {{Form::button('<i class="fa fa-trash"></i> DELETE', array('type' => 'submit', 'class' => 'btn btn-danger'))}}
+                                    {!! Form::close() !!}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </td>
-            </tr>
-
-            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="myModalLabel">Warning</h4>
-                        </div>
-                        <div class="modal-body">
-                            Are you sure you want to delete {{ $grant->grant_name }}? This action cannot be undone.
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            {!! Form::open(array(
-                            'style' => 'display: inline-block;',
-                            'method' => 'DELETE',
-                            'route' => ['grants.destroy', $grant->id])) !!}
-                            {{Form::button('<i class="fa fa-trash"></i> DELETE', array('type' => 'submit', 'class' => 'btn btn-danger'))}}
-                            {!! Form::close() !!}
-                        </div>
-                    </div>
-                </div>
-            </div>
-            @endforeach
+                @endforeach
             @else
             <tr>
                 <td colspan="7">No entries in table</td>
@@ -132,5 +130,7 @@
     $(".panel").fadeIn("fast");
 
 </script>
-
+<script>
+    window.route_mass_crud_entries_destroy = '{{ route('vendors.mass_destroy') }}';
+</script>
 @endsection
