8ca643a8892aecc71d39027646279e6293a9e5a0
chanikyasairam <cgopisetty@unomaha.edu>
2016-11-25 03:51:30 -0600

    History changes
    History changes has been accommodated

--- app/Http/Controllers/VehiclesController.php
+++ app/Http/Controllers/VehiclesController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Vehicle;
+use App\Vehiclehis;
 use App\UnitType;
 use App\AllAsset;
+use App\Station;
+use App\Grant;
+use App\Status;
+use App\Vendor;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreVehiclesRequest;
 use App\Http\Requests\UpdateVehiclesRequest;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class VehiclesController extends Controller
 {
@@ -77,6 +83,7 @@ class VehiclesController extends Controller
 
         
         $vehicle = Vehicle::findOrFail($id);
+        //dd($vehicle);
         return view('vehicles.edit', compact('vehicle', '') + $relations);
     }
 
@@ -89,11 +96,91 @@ class VehiclesController extends Controller
      */
     public function update(UpdateVehiclesRequest $request, $id)
     {
+        //dd($request);
         $vehicle = Vehicle::findOrFail($id);
-        $vehicle->update($request->all());
 
 
-        return redirect()->route('vehicles.index');
+        //start history code
+        //storing all foreing key id s in variables
+        $stationid=$vehicle->station_id;
+       $statusid=$vehicle->status_id;
+       $grantid=$vehicle->grant_id;
+       $unittypeid=$vehicle->unittype_id;
+       $vendorid=$vehicle->vendor_id;
+
+
+       
+       //finding relavent names against each primary key and storing it in variables
+       $station = Station::find($stationid);
+       $grant = Grant::find($grantid);
+       $status = Status::find($statusid);
+       $unit = Unittype::find($unittypeid);
+       $vendor = Vendor::find($vendorid);
+
+       //if else code to handle null values
+
+       if($grant)
+       {
+        $grant_name=$grant->grant_name;
+      
+       }
+       else
+       {
+        $grant_name=null;
+       }
+
+       if($station)
+       {
+        $station_name=$station->station_name;
+       }
+       else
+       {
+        $station_name=null;
+       }
+
+       if($status)
+       {
+        $status_name=$status->status;
+      
+       }
+       else
+       {
+        $status_name=null;
+       }
+      
+      if($unit)
+       {
+        $unittype_name=$unit->name;
+      
+       }
+       else
+       {
+        $unittype_name=null;
+       }
+      
+      if($vendor)
+       {
+        $vendor_name=$vendor->vendor_name;
+      
+       }
+
+       else
+       {
+        $vendor_name=null;
+       }
+       
+       //DB insert query to insert history values to new table
+
+ \DB::table('vehiclehis')->insert(
+            ['vehicle_id' => $vehicle->id, 'van' => $vehicle->van, 'make' => $vehicle->make, 'model' => $vehicle->model,  'year' => $vehicle->year, 'station_id' => $vehicle->station_id, 'name' => $station_name,'grant_id' => $vehicle->grant_id, 'grant_name' => $grant_name,'status_id' => $vehicle->status_id, 'status' => $status_name,'unittype_id' => $vehicle->unittype_id, 'unittype_name' => $unittype_name,'vendor_id' => $vehicle->vendor_id, 'vendor_name' => $vendor_name, "created_at" =>  \Carbon\Carbon::now(), "updated_at" => \Carbon\Carbon::now()  ] 
+        );
+        
+
+     //end history code.
+
+ 
+      $vehicle->update($request->all());
+      return redirect()->route('vehicles.index');
     }
 
     /**
@@ -116,7 +203,10 @@ class VehiclesController extends Controller
         //dd($relations);
 
         $vehicle = Vehicle::findOrFail($id);
-        return view('vehicles.show', compact('vehicle') + $relations);
+        //below one line code is for storing all history related to the $id in variable, which is to be used to display in show page.
+        $vehiclehis2 = Vehiclehis::where('vehicle_id', $id)->get();
+        //dd($vehiclehis2);
+        return view('vehicles.show', compact('vehicle', 'vehiclehis2') + $relations);
     }
 
 public function reassign(Request $request)
